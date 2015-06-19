42465725ef1d826677ccf7e1dbb363c09c49e759
Kyle Newswanger <knews12@comcast.net>
2015-06-19 09:47:31 -0400

    Added missed files from last commit

---
+++ TravelingSalesman/SmallestTour.java
@@ -0,0 +1,35 @@
+public class SmallestTour 
+{
+    public static void main(String[] args)
+    {
+        int w = StdIn.readInt();
+        int h = StdIn.readInt();
+        StdDraw.setCanvasSize(w, h);
+        StdDraw.setXscale(0, w);
+        StdDraw.setYscale(0, h);
+
+        // turn on animation mode
+        StdDraw.show(0);
+        
+        Tour tour = new Tour();
+        
+        while(!StdIn.isEmpty())
+        {
+            double x = StdIn.readDouble();
+            double y = StdIn.readDouble();
+            Point p = new Point(x, y);
+            
+            tour.insertSmallest(p);
+            
+            //StdDraw.clear();
+            //tour.draw();
+            //StdDraw.show(50);
+        }
+        
+        tour.draw();
+        StdDraw.show(0);
+        
+        System.out.println("Distance = " + String.valueOf(tour.distance()));
+        tour.draw();
+    }
+}
